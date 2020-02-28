dde1ba6fc60a586c3671cf8e23a2cf4d06692246
iterobin <iterobin@outlook.com>
2020-02-28 16:29:37 +0800

    搭建前端页面框架

---
+++ RestTester/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestTester.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public int ApiCount { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public bool IsShared { get; set; }
+    }
+}
